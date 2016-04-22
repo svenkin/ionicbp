d10cdf9b7cf8331fadd8327fedbfec38a18ff0c8
Daniel Simon <daniel.s1mon@icloud.com>
2016-04-22 17:31:29 +0200

    added plugins

--- www/app/menu/menu.js
+++ www/app/menu/menu.js
@@ -1,24 +1,23 @@
 angular.module('app.menu', []).config(function ($stateProvider) {
-        $stateProvider.state('app', {
-            url: '/app',
-            abstract: true,
-            templateUrl: 'app/menu/menu.html',
-            controller: 'AppCtrl'
-        })
+    $stateProvider.state('app', {
+      url: '/app',
+      abstract: true,
+      templateUrl: 'app/menu/menu.html',
+      controller: 'AppCtrl'
     })
-    .controller('AppCtrl', function ($scope, $log, localStorageService, $ionicContentBanner, $state, $rootScope, shoppingCart) {
-        $rootScope.$on('item-added-cart',
-            function () {
-                $scope.price = shoppingCart.getFullPrice();
-            });
-        $scope.logout = function () {
-            localStorageService.set('user', '');
-            $ionicContentBanner.show({
-                autoClose: 3000,
-                text: ['Erfolgreich ausgeloggt!'],
-                type: 'info'
-            });
-            $state.go('login');
-        }
-
-    });
+  })
+  .controller('AppCtrl', function ($scope, $log, localStorageService, $ionicContentBanner, $state, $rootScope, shoppingCart) {
+    $rootScope.$on('item-added-cart',
+      function () {
+        $scope.price = shoppingCart.getFullPrice();
+      });
+    $scope.logout = function () {
+      localStorageService.set('user', '');
+      $ionicContentBanner.show({
+        autoClose: 3000,
+        text: ['Erfolgreich ausgeloggt!'],
+        type: 'info'
+      });
+      $state.go('login');
+    }
+  });
