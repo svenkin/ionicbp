7adb02984aab0dd29c797c204a0ccf8839709da1
Sven Kinzel <sven@hegerkinzel.de>
2016-04-22 16:53:34 +0200

    added new order service

--- www/app/menu/menu.js
+++ www/app/menu/menu.js
@@ -1,20 +1,21 @@
-angular.module('app.menu',[]).config(function ($stateProvider) {
-    $stateProvider.state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'app/menu/menu.html',
-    controller: 'AppCtrl'
-  })
-})
-.controller('AppCtrl', function ($scope, $log, localStorageService, $ionicContentBanner, $state) {
-  $scope.logout = function () {
-    localStorageService.set('user', '');
-    $ionicContentBanner.show({
-      autoClose: 3000,
-      text: ['Erfolgreich ausgeloggt!'],
-      type: 'info'
-    });
-    $state.go('login');
-  }
+angular.module('app.menu', []).config(function ($stateProvider) {
+        $stateProvider.state('app', {
+            url: '/app',
+            abstract: true,
+            templateUrl: 'app/menu/menu.html',
+            controller: 'AppCtrl'
+        })
+    })
+    .controller('AppCtrl', function ($scope, $log, localStorageService, $ionicContentBanner, $state, $ionicSideMenuDelegate) {
+        
+        $scope.logout = function () {
+            localStorageService.set('user', '');
+            $ionicContentBanner.show({
+                autoClose: 3000,
+                text: ['Erfolgreich ausgeloggt!'],
+                type: 'info'
+            });
+            $state.go('login');
+        }
 
-});
+    });
--- www/app/services/newOrder.js
+++ www/app/services/newOrder.js
@@ -1,22 +1,35 @@
-angular.modul('app').factory('newOrder', function ($q,BaseUrl RequestFactory) {
+angular.module('app').factory('newOrder', function ($q,$log,BaseUrl, RequestFactory,$filter,localStorageService) {
     var service = {};
     var baseUrl = BaseUrl;
-    serice.send = function () {
+    service.send = function (customerId) {
         var q = $q.defer();
         var order = localStorageService.get('shopping-cart') || 'fail';
-        if (order === 'fail') {
+        var mbId = localStorageService.get('user').fieldWorkerId || 'fail';
+        if (order === 'fail' || mbId === 'fail') {
             q.reject('Keine Items im Einkaufswagen');
             return q.promise;
         } else {
+            
+            var orderDate = $filter('date')(new Date(),'yyyy-MM-dd');
+            var orderData = "";
+            angular.forEach(order,function(value){
+                orderData += value.itemId+":"+value.quantity+";"
+            });
+            $log.log('Orderdata',orderData);
             RequestFactory.get(baseUrl+'api/CreateOrder',{reqParams : {params : {
+                customerID : customerId,
+                fieldWorkerID : mbId,
+                orderDate : orderDate,
+                orderData : orderData
                 
             }}}).then(function(suc){
-                
+                $log.log('erstellt' , suc);
+                q.resolve(suc);
             },function(err){
-                
+                q.reject(err);
             })
         }
-
+        return q.promise;
     }
 
     return service;
