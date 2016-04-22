5fb5da28033826868a33761d47dab264cf7c2bc2
Sven Kinzel <sven@hegerkinzel.de>
2016-04-22 14:20:42 +0200

    wip

--- www/app/dashboard/dashboard.js
+++ www/app/dashboard/dashboard.js
@@ -9,12 +9,13 @@ angular.module('app.dashboard', []).config(function ($stateProvider) {
             }
         })
     })
-    .controller('DashboardCtrl', function ($scope, $log, UserData,Orders,Customer) {
+    .controller('DashboardCtrl', function ($scope, $log, UserData,Orders,Customer,$filter) {
     $scope.data = {};
         $scope.test = function () {
 //            Customer.getCustomerById(20002);
             Orders.getAllOrders(10002).then(function (ord) {
-                $scope.data.orders = ord;
+                var ordered = $filter('orderBy')(ord, 'orderDate',true);
+                $scope.data.orders = ordered.splice(0,5);
             }, function (err) {
                 $log.log(err);
             })
