749223bca2721afe1b403cd6d959e29bd64eef29
Sven Kinzel <sven@hegerkinzel.de>
2016-04-22 15:20:11 +0200

    added momentjs

--- www/app/dashboard/dashboard.js
+++ www/app/dashboard/dashboard.js
@@ -9,13 +9,36 @@ angular.module('app.dashboard', []).config(function ($stateProvider) {
             }
         })
     })
-    .controller('DashboardCtrl', function ($scope, $log, UserData,Orders,Customer,$filter) {
-    $scope.data = {};
+    .controller('DashboardCtrl', function ($scope, $log, UserData, Orders, Customer, $filter, $ionicLoading, $timeout) {
+        $scope.data = {};
         $scope.test = function () {
-//            Customer.getCustomerById(20002);
+            $ionicLoading.show({
+                template: 'Loading...'
+            });
+            //            Customer.getCustomerById(20002);
             Orders.getAllOrders(10002).then(function (ord) {
-                var ordered = $filter('orderBy')(ord, 'orderDate',true);
-                $scope.data.orders = ordered.splice(0,5);
+                var ordered = $filter('orderBy')(ord, 'orderDate', true);
+                $scope.data.orders = ordered.splice(0, 5);
+                $log.log($scope.data.orders);
+                $timeout(function () {
+                    $ionicLoading.hide();
+                }, 500)
+
+            }, function (err) {
+                $log.log(err);
+                $ionicLoading.hide();
+            })
+        }
+
+        $scope.refresh = function () {
+            $scope.data.orders = [];
+            Orders.getAllOrders(10002).then(function (ord) {
+                var ordered = $filter('orderBy')(ord, 'orderDate', true);
+                $scope.data.orders = ordered.splice(0, 5);
+                $timeout(function () {
+                    $scope.$broadcast('scroll.refreshComplete');
+                }, 500)
+
             }, function (err) {
                 $log.log(err);
             })
