d3bb0e7e572df7919974cfa12a716ce3e5800762
Sven Kinzel <sven@hegerkinzel.de>
2016-04-22 17:01:31 +0200

    orderes ordered by date

--- www/app/dashboard/dashboard.js
+++ www/app/dashboard/dashboard.js
@@ -25,8 +25,8 @@ angular.module('app.dashboard', []).config(function ($stateProvider) {
             });
             //            Customer.getCustomerById(20002);
             Orders.getAllOrders(10002).then(function (ord) {
-                var ordered = $filter('orderBy')(ord, 'orderDate', true);
-                $scope.data.orders = ordered.splice(ordered.length-5, 5);
+                var ordered = $filter('orderBy')(ord, 'orderDateRaw', true);
+                $scope.data.orders = ordered.splice(0, 5);
                 $log.log($scope.data.orders);
                 $timeout(function () {
                     $ionicLoading.hide();
@@ -40,8 +40,8 @@ angular.module('app.dashboard', []).config(function ($stateProvider) {
         $scope.refresh = function () {
             $scope.data.orders = [];
             Orders.getAllOrders(10002).then(function (ord) {
-                var ordered = $filter('orderBy')(ord, 'orderDate', true);
-                $scope.data.orders = ordered.splice(ordered.length-5, 5);
+                var ordered = $filter('orderBy')(ord, 'orderDateRaw', true);
+                $scope.data.orders = ordered.splice(0, 5);
                 $timeout(function () {
                     $scope.$broadcast('scroll.refreshComplete');
                 }, 500)
