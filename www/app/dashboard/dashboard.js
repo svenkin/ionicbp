620f04f0b5fb8a34b738fe1f65309e43584ce440
Sven Kinzel <sven@hegerkinzel.de>
2016-04-22 20:56:10 +0200

    wip

--- www/app/dashboard/dashboard.js
+++ www/app/dashboard/dashboard.js
@@ -9,33 +9,32 @@ angular.module('app.dashboard', []).config(function ($stateProvider) {
             }
         })
     })
-    .controller('DashboardCtrl', function ($scope, $log, UserData, Orders, Customer, $filter, $ionicLoading, $timeout, newOrder, localStorageService, $state,$rootScope) {
+    .controller('DashboardCtrl', function ($scope, $log, UserData, Orders, Customer, $filter, $ionicLoading, $timeout, newOrder, localStorageService, $state, $rootScope) {
+        var stateBefore = '';
         $rootScope.$on('$stateChangeSuccess', function (ev, to, toParams, from, fromParams) {
-            console.log(from);
             if (from.name === 'login') {
+                stateBefore = from.name;
                 loadData();
+            }else{
+                stateBefore = '';
             }
         });
         $scope.data = {};
-        //        (function(){
-        //            newOrder.send(20010).then(function(){
-        //                
-        //            },function(){
-        //                
-        //            })
-        //        })();
 
         function loadData() {
-            $ionicLoading.show({
-                template: 'Loading...'
-            });
-            //            
-            //            Customer.getCustomerById(20002);
+            console.log('Lade daten')
+            $scope.$on('$ionicView.beforeEnter', function () {
+                if (stateBefore === 'login') {
+                    console.log('show')
+                    $ionicLoading.show({
+                        template: '<div style="text-align:center">Lade Daten...<br/><ion-spinner icon="spiral"></ion-spinner></div>'
+                    });
+                }
+            })
             var id = localStorageService.get('user').fieldWorkerId || 'fail';
             Orders.getAllOrders(id).then(function (ord) {
                 var ordered = $filter('orderBy')(ord, 'orderId', true);
                 $scope.data.orders = ordered.splice(0, 5);
-                $log.log($scope.data.orders);
                 $timeout(function () {
                     $ionicLoading.hide();
                 }, 500)
