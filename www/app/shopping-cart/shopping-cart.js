0b8b0aa634f4961b9c8e757168eea6985847f11a
Sven Kinzel <sven@hegerkinzel.de>
2016-04-23 09:43:10 +0200

    fixxed merge

--- www/app/shopping-cart/shopping-cart.js
+++ www/app/shopping-cart/shopping-cart.js
@@ -1,76 +1,48 @@
 angular.module('app.shoppingCart', []).config(function ($stateProvider) {
-    $stateProvider.state('app.shoppingCart', {
-      url: '/shopping-cart',
-      views: {
-        'menuContent': {
-          templateUrl: 'app/shopping-cart/shopping-cart.html',
-          controller: 'ShoppingCartCtrl'
-        }
-      }
+        $stateProvider.state('app.shoppingCart', {
+            url: '/shopping-cart',
+            views: {
+                'menuContent': {
+                    templateUrl: 'app/shopping-cart/shopping-cart.html',
+                    controller: 'ShoppingCartCtrl'
+                }
+            }
+        })
     })
-  })
-  .controller('ShoppingCartCtrl', function ($scope, $log, $ionicSideMenuDelegate, $timeout, localStorageService, shoppingCart, $ionicModal, Customer, newOrder, $ionicLoading, $ionicContentBanner, $state) {
-    $scope.data = {};
-    $scope.data.price = shoppingCart.getFullPrice();
-    $scope.data.cart = localStorageService.get('shopping-cart');
-    $ionicSideMenuDelegate.canDragContent(true);
-    var customerId = 0;
+    .controller('ShoppingCartCtrl', function ($scope, $log, $timeout, localStorageService, shoppingCart, $ionicModal, Customer, newOrder, $ionicLoading, $ionicContentBanner, $state) {
+        $scope.data = {};
+        $scope.data.price = shoppingCart.getFullPrice();
+        $scope.data.cart = localStorageService.get('shopping-cart');
+        var customerId = 0;
 
-    $scope.changeQuantity = function (id, quantity) {
-      if (quantity < 0 || typeof quantity != 'number') {
-        $scope.data.cart[id].quantity = 0;
-      }
-      localStorageService.set('shopping-cart', $scope.data.cart);
-      $scope.data.price = shoppingCart.getFullPrice();
-    };
-    $scope.remove = function (id) {
-      delete $scope.data.cart[id];
-      localStorageService.set('shopping-cart', $scope.data.cart);
-      $scope.data.price = shoppingCart.getFullPrice();
-    };
+        $scope.changeQuantity = function (id, quantity) {
+            if (quantity < 0 || typeof quantity != 'number') {
+                $scope.data.cart[id].quantity = 0;
+            }
+            localStorageService.set('shopping-cart', $scope.data.cart);
+            $scope.data.price = shoppingCart.getFullPrice();
+        };
+        $scope.remove = function (id) {
+            delete $scope.data.cart[id];
+            localStorageService.set('shopping-cart', $scope.data.cart);
+            $scope.data.price = shoppingCart.getFullPrice();
+        };
 
-    $ionicModal.fromTemplateUrl('app/shopping-cart/sendOrder/sendOrder.html', {
-      scope: $scope,
-      animation: 'slide-in-up'
-    }).then(function (modal) {
-      $scope.modal = modal;
-    });
+        $ionicModal.fromTemplateUrl('app/shopping-cart/sendOrder/sendOrder.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
 
-    $scope.sendOrder = function () {
-      $scope.data.user = localStorageService.get('user');
-      $scope.modal.show();
-    }
-    $scope.closeModal = function () {
-      $scope.modal.hide();
-    };
-    // Cleanup the modal when we're done with it!
-    $scope.$on('$destroy', function () {
-      $scope.modal.remove();
-    });
-    // Execute action on hide modal
-    $scope.$on('modal.hidden', function () {
-      // Execute action
-    });
-    // Execute action on remove modal
-    $scope.$on('modal.removed', function () {
-      // Execute action
-    });
-    $scope.data.style = {};
-    $scope.data.loaded = false;
-    $scope.data.compName = "Kunden-Id";
-    $scope.send = function () {
-      if ($scope.data.custfound) {
-        newOrder.send(customerId).then(function (suc) {
-          $log.log(suc);
-          localStorageService.remove('shopping-cart');
-          $ionicContentBanner.show({
-            autoClose: 3000,
-            text: ['Erfolgreich Bestellung versendet'],
-            type: 'success',
-            cancelOnStateChange: false
-          });
-          $scope.modal.hide();
-          $state.go('app.dashboard');
+        $scope.sendOrder = function () {
+            $scope.data.user = localStorageService.get('user');
+            $scope.modal.show();
+        }
+        $scope.data.style = {};
+        $scope.data.loaded = false;
+        $scope.data.compName = "Kunden-Id";
+
         $scope.closeModal = function () {
             $scope.modal.hide();
         };
@@ -98,40 +70,36 @@ angular.module('app.shoppingCart', []).config(function ($stateProvider) {
                         autoClose: 3000,
                         text: ['Erfolgreich Bestellung versendet'],
                         type: 'success',
-                        cancelOnStateChange : false
+                        cancelOnStateChange: false
                     });
                     $scope.modal.hide();
                     $state.go('app.dashboard');
 
-        }, function (err) {
-          $ionicContentBanner.show({
-            autoClose: 3000,
-            text: [err],
-            type: 'error'
-          })
-        });
-      } else {
-        $log.warn('Bestellung wurde nicht versendet!');
-        $scope.data.style = {
-          border: "1px solid red"
+                }, function (err) {
+                    $ionicContentBanner.show({
+                        autoClose: 3000,
+                        text: [err],
+                        type: 'error'
+                    })
+                });
+            } else {
+                $log.warn('Bestellung wurde nicht versendet!');
+                $scope.data.style = {
+                    border: "1px solid red"
+                }
+                $scope.data.loaded = true;
+                $scope.data.custfound = false;
+                $scope.data.compName = "Kunden-Id";
+            }
         }
-        $scope.data.loaded = true;
-        $scope.data.custfound = false;
-        $scope.data.compName = "Kunden-Id";
-      }
-    }
-    $scope.$watch('data.custId', function (val) {
-      if (val !== null && val !== undefined && val !== 0) {
-        $ionicLoading.show({
-          template: '<div style="text-align:center">Überprüfe Kunden-Id...<br/><ion-spinner icon="spiral"></ion-spinner></div>'
         $scope.$watch('data.custId', function (val) {
             if (val !== null && val !== undefined && val !== 0) {
                 $ionicLoading.show({
                     template: '<div style="text-align:center">Überprüfe Kunden-Id...<br/><ion-spinner icon="spiral"></ion-spinner></div>'
-                });
+                })
+
                 Customer.getCustomerById(val).then(function (suc) {
                     console.log(suc);
-                    $scope.data.customer = suc;
                     customerId = suc.customerId;
                     $timeout(function () {
                         $scope.data.style = {
@@ -145,7 +113,6 @@ angular.module('app.shoppingCart', []).config(function ($stateProvider) {
 
                 }, function (err) {
                     console.log(err);
-                    $scope.data.customer = {};
                     $timeout(function () {
                         $scope.data.style = {
                             border: "1px solid red"
@@ -157,7 +124,6 @@ angular.module('app.shoppingCart', []).config(function ($stateProvider) {
                     }, 500)
                 })
             } else {
-                $scope.data.customer = {};
                 $scope.data.loaded = false;
                 $scope.data.style = {
                     border: "none"
@@ -165,38 +131,5 @@ angular.module('app.shoppingCart', []).config(function ($stateProvider) {
                 $scope.data.compName = "Kunden-Id";
             }
         });
-        Customer.getCustomerById(val).then(function (suc) {
-          console.log(suc);
-          customerId = suc.customerId;
-          $timeout(function () {
-            $scope.data.style = {
-              border: "1px solid green"
-            };
-            $ionicLoading.hide();
-            $scope.data.loaded = true;
-            $scope.data.custfound = true;
-            $scope.data.compName = suc.companyName;
-          }, 500)
 
-        }, function (err) {
-          console.log(err);
-          $timeout(function () {
-            $scope.data.style = {
-              border: "1px solid red"
-            }
-            $ionicLoading.hide();
-            $scope.data.loaded = true;
-            $scope.data.custfound = false;
-            $scope.data.compName = "Kunden-Id";
-          }, 500)
-        })
-      } else {
-        $scope.data.loaded = false;
-        $scope.data.style = {
-          border: "none"
-        };
-        $scope.data.compName = "Kunden-Id";
-      }
     });
-
-  });
